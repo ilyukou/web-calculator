fb559e07186012f55880baf7bb8b32e74b548c91
Ilya Ilyukou <ilyailyukou@gmail.com>
2018-06-13 12:53:56 +0200

    add cos/sin/tan/ln/e v1.0.1

--- scripts/script.js
+++ scripts/script.js
@@ -105,11 +105,57 @@ function symbolOperation(symbol){
             input[i] = input[i].value;
             mathOperation();
             break;
+
+        // ver 1.0.1
+        case "sin":
+            input[1] = "sin";
+            input[i] = input[i].value;
+            i = i+2;
+            // num 2 input
+            input[i] =  document.getElementById("inputNumber");
+            input[i].value = 0;
+            break;
+
+        case "cos":
+            input[1] = "cos";
+            input[i] = input[i].value;
+            i = i+2;
+            // num 2 input
+            input[i] =  document.getElementById("inputNumber");
+            input[i].value = 0;
+            break;
+
+        case "tan":
+            input[1] = "tan";
+            input[i] = input[i].value;
+            i = i+2;
+            // num 2 input
+            input[i] =  document.getElementById("inputNumber");
+            input[i].value = 0;
+            break;
+
+        case "ln":
+            input[1] = "ln";
+            input[i] = input[i].value;
+            i = i+2;
+            // num 2 input
+            input[i] =  document.getElementById("inputNumber");
+            input[i].value = 0;
+            break;
+
+        case "e":
+            input[1] = "e";
+            input[i] = input[i].value;
+            i = i+2;
+            // num 2 input
+            input[i] =  document.getElementById("inputNumber");
+            input[i].value = 0;
+            break;
     }
 }
 
 function mathOperation(objValue) {
-    console.log('mathOperation');
+
 
     var num1 = input[0];
     var num2 = input[2];
@@ -143,6 +189,32 @@ function mathOperation(objValue) {
             break;
         case "x":
             res = num1 * num2;
+            resInput.value = res;
+            break;
+
+        // ver 1.0.1
+        case "sin":
+            res = Math.sin(num2);
+            resInput.value = res;
+            break;
+
+        case "cos":
+            res = Math.cos(num2);
+            resInput.value = res;
+            break;
+
+        case "tan":
+            res = Math.tan(num2);
+            resInput.value = res;
+            break;
+
+        case "ln":
+            res = Math.log(num2);
+            resInput.value = res;
+            break;
+
+        case "e":
+            res = Math.exp(num2);
             resInput.value = res;
             break;
 
